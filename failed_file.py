480a59b6e9c33be3d01c5d0a83e2f94ea28cac23
Muhammad Usama <usaman3244015@gmail.com>
2025-04-28 16:18:04 +0500

    Add files via upload

---
+++ failed_file.py
@@ -0,0 +1,11 @@
+import pdb
+
+def multiply_numbers(a, b):    
+    return a * b    
+
+# some random debug statement
+pdb.set_trace()
+
+private_key = """-----BEGIN PRIVATE KEY-----
+abc123
+-----END PRIVATE KEY-----"""
---
+++ passed_file.py
@@ -0,0 +1,5 @@
+def add_numbers(a, b):
+    return a + b
+
+def say_hello(name):
+    print(f"Hello, {name}!")
